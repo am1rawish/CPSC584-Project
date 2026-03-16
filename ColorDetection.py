cd5800f7b11505626ea007c9554c5fa3ac7bee9f
Your Name <amirawish16@gmail.com>
2026-03-16 17:49:50 -0600

    Change alignment and method to move forward

--- ColorDetection.py
+++ ColorDetection.py
@@ -11,7 +11,7 @@ def detect_color():
     for color in colors:
 
         Vilib.color_detect(color)
-        sleep(3)
+        sleep(2)
 
         count = Vilib.detect_obj_parameter.get('color_n', 0)
 
@@ -22,32 +22,51 @@ def detect_color():
 
 def align_to_color(color):
 
+    is_aligned = False
 
     Vilib.color_detect(color)
 
     while True:
 
         count = Vilib.detect_obj_parameter.get('color_n',0)
+        
 
         if count == 0:
             print("Lost color")
-            return False
+            is_aligned = False
+            return is_aligned
 
         x = Vilib.detect_obj_parameter.get('color_x')
+        w = Vilib.detect_obj_parameter.get('color_w')
 
-        print("Color position:", x)
+        print("Color position start:", x)
+        print("Color distance start:", w)
 
-        if x < 120:
+        if x < 250:
             Bala7a.do_action('turn left angle',1,80)
+            print("Color position l:", x)
 
-        elif x > 200:
+        if x > 400:
             Bala7a.do_action('turn right angle',1,80)
+            print("Color position right:", x)
 
-        else:
-            print("Aligned!")
-            return True
+        if w < 65:
+            Bala7a.do_action('turn left', 1, 80)
+            Bala7a.do_action('forward', 1, 80)
+            print("Color distance f:", w)
+        
+        if w > 85:
+            Bala7a.do_action('backward', 1, 80)
+            print("Color distance b:", w)
 
-        sleep(0.4)
+        elif 200 <= x <= 400 and 65 <= w <= 85:
+            print("Final distance:", w)
+            print("Final position:", x)
+            is_aligned = True
+            print("Aligned with color!")
+            break
+
+    return is_aligned
 
 
 def main():
@@ -60,7 +79,7 @@ def main():
     Vilib.camera_start(vflip=False, hflip=False)
     Vilib.display(local=True, web=True)
 
-    sleep(3)
+    sleep(1)
 
     while True:
 
@@ -68,20 +87,19 @@ def main():
 
         if color is None:
             print("No color detected, scanning...")
-            Bala7a.do_action('turn left angle',1,speed)
-            sleep(3)
+            Bala7a.do_action('turn left',1,speed)
 
         else: 
             print(f"Detected color: {color}")
             
             aligned = align_to_color(color)
 
-            if aligned:
-                print(f"Moving towards {color} path")
-                Bala7a.do_action('forward', 6, speed)
+            if not aligned:
+              print("Failed to align with color, scanning again...")
 
-            break
-
+            else:
+                print("Successfully aligned with color!")
+                break
 
 if __name__ == "__main__":
     main()
