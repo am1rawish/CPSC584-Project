a7305f5fcd791d9f0e84ee9ef16439500f44c4a5
Your Name <amirawish16@gmail.com>
2026-04-08 18:15:10 -0600

    stuff from last commit. (forgot to stage changes)

--- read_cards.py
+++ read_cards.py
@@ -2,6 +2,13 @@ from vilib import Vilib
 from time import sleep
 from picrawler import Picrawler
 from twist import twist
+#import preset_actions
+import sys
+import os
+
+sys.path.append(os.path.expanduser("~/picrawler/examples"))
+
+import preset_actions
 
 # read cards in treasure/danger boxes
 # if card is blue --> answer is correct --> go to next area
@@ -13,26 +20,40 @@ def detect_color():
 
     colors =  ["red", "blue"]
 
-    for color in colors:
+    while True:
+        for color in colors:
 
-        Vilib.color_detect(color)
-        sleep(2)
+            Vilib.color_detect(color)
+            sleep(3)
 
-        count = Vilib.detect_obj_parameter.get('color_n', 0)
+            count = Vilib.detect_obj_parameter.get('color_n', 0)
 
-        if count > 0:
-            return color
+            if count > 0:
+                if color == "red":
+                    answer = False
+                    break
 
-    return None
+                if color == "blue":
+                    answer = True
+                    break
+            else:
+                print(f"No card detected, scanning again...")
+                continue 
 
-def react(detected_color):
+    return answer
 
-    if detected_color == "red":
+def react(answer_value):
+
+    if answer_value == False:
         print("reacting to wrong answer")
+        preset_actions.shake_head()
+        preset_action.look_up()
+        preset_actions.fighting()
+        # sounds here
         #movements here
        
 
-    elif detected_color == "blue":
+    elif answer_value == True:
         #movements here
         i = 0
         while i <= 10:
@@ -52,7 +73,7 @@ def main():
             color = detect_color()
 
             if color is None:
-                print("No card detected")
+                print("No card detected, scanning")
                 continue
             else:
                 react(color)
@@ -63,5 +84,13 @@ def main():
     except KeyboardInterrupt:
         print("\nCtrl+C detected, exiting safely...")
 
+
+def test():
+    Vilib.camera_start(vflip=False, hflip=False)
+    Vilib.display(local=True, web=True)
+
+    detected_color = detect_color()
+    react(detected_color)
+
 if __name__ == "__main__":
-    main()
+    test()
